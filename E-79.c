b649203dc5c2de70a4d3184946abc35c66c16e0d
daszizgreat <120002786+daszizgreat@users.noreply.github.com>
2023-05-12 21:38:44 +0530

    E-79.c

--- E-79.c
+++ E-79.c
@@ -23,51 +23,43 @@ int main()
     CURLcode res;
     char *data = NULL;
     cJSON *root, *weather, *weather_desc, *main_info, *temperature, *humidity;
-    char *city = "YKOLKATA"; // Replace with your city name
+    char *city = "YKOLKATA"; 
     char url[100];
 
-    // Create URL for API request
-    sprintf(url, "ENTER API AND LINK", city); // Replace with your API key
+    sprintf(url, "ENTER API AND LINK", city); 
 
     // Initialize CURL
     curl_global_init(CURL_GLOBAL_DEFAULT);
     curl = curl_easy_init();
     if (curl) {
-        // Set API URL
         curl_easy_setopt(curl, CURLOPT_URL, url);
 
-        // Set callback function to retrieve response data
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
 
-        // Perform the request
         res = curl_easy_perform(curl);
         if (res != CURLE_OK) {
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
             return 1;
         }
 
-        // Parse JSON response
         root = cJSON_Parse(data);
         if (root == NULL) {
             printf("Failed to parse JSON data.\n");
             return 1;
         }
 
-        // Retrieve weather information
         weather = cJSON_GetObjectItemCaseSensitive(root, "weather");
         weather_desc = cJSON_GetObjectItemCaseSensitive(cJSON_GetArrayItem(weather, 0), "description");
         main_info = cJSON_GetObjectItemCaseSensitive(root, "main");
         temperature = cJSON_GetObjectItemCaseSensitive(main_info, "temp");
         humidity = cJSON_GetObjectItemCaseSensitive(main_info, "humidity");
 
-        // Display weather information
         printf("City: %s\n", city);
         printf("Weather: %s\n", cJSON_GetStringValue(weather_desc));
         printf("Temperature: %.2f degrees Celsius\n", cJSON_GetNumberValue(temperature) - 273.15);
         printf("Humidity: %d%%\n", cJSON_GetNumberValue(humidity));
 
-        // Clean up
         cJSON_Delete(root);
         curl_easy_cleanup(curl);
         curl_global_cleanup();
